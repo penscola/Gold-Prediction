c638377a9285a92632c496bb2bfe59a21193d277
penscola <penscolashackletonfelix@gmail.com>
2023-11-08 19:14:22 +0300

    created my predict and train file for the model

---
+++ src/predict.py
@@ -0,0 +1,32 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+
+model_file = '../../model/Random-Forest-Regressor.pkl'
+
+with open(model_file, 'rb') as f_in:
+    scaler, model = pickle.load(f_in)
+
+app = Flask('price')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    X = scaler.transform([customer])
+    y_pred = model.predict(X)
+    price = y_pred >= 0.5
+
+    result = {
+        'price_probability': float(y_pred),
+        'price': bool(price)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ src/train.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+
+# importing the dataset
+df = pd.read_csv('../../data/gld_price_data.csv')
+print("Data Imported Successfully ✅")
+
+# Removing the date column
+df = df.drop(columns =['Date'],axis=1)
+
+# splitting the dataset
+X = df.drop(columns=['GLD'],axis=1)
+y = df['GLD']
+X_train,X_test,y_train,y_test = train_test_split(X,y , test_size=0.2 , random_state=42)
+
+# Feature Scaling
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train,y_train)
+X_test_scaled  =scaler.transform(X_test)
+
+# model
+rf1 = RandomForestRegressor()
+
+# Hyperparameter Tuning
+n_estimators = [20,60,100,120]
+max_features = [0.2,0.6,1.0]
+max_depth = [2,8,None]
+max_samples = [0.5,0.75,1.0]
+
+param_grid = { 'n_estimators' : n_estimators,
+               'max_features' : max_features,
+               'max_depth' : max_depth,
+               'max_samples' : max_samples
+}
+rf1_grid = GridSearchCV(estimator = rf1,
+                       param_grid = param_grid,
+                       cv=5,
+                       verbose =2,
+                       n_jobs = -1)
+
+# training the model
+rf1_grid.fit(X_train_scaled,y_train)
+
+# saving the model
+output_file = '../../model/Random-Forest-Regressor.pkl'
+
+with open(output_file, 'wb') as f_out:
+    pickle.dump((scaler, rf1_grid), f_out)
